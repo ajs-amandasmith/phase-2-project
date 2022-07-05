6f5388bcab8ed9162259eba22969c0f7e4e9474c
Amanda Smith <ajs.amandasmith@gmail.com>
2022-07-05 16:57:38 -0500

    Added db.json file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,30 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  const navStyle = {
+    display: "inline-block",
+    width: "100px",
+    height: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  }
+
+  
   return (
     <div>
-      <NavLink to="/">
+      <NavLink to="/" style={navStyle}>
         Home
       </NavLink>
-      <NavLink to="to-read">
+      <NavLink to="to-read" style={navStyle}>
         To Read List
       </NavLink>
-      <NavLink to="have-read">
+      <NavLink to="have-read" style={navStyle}>
         Have Read List
       </NavLink>
-      <NavLink to="/currently-reading">
+      <NavLink to="/currently-reading" style={navStyle}>
         Currently Reading List
       </NavLink>
     </div>
