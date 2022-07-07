88b8ebd020c0635e964d2c6c57d3e90db66d6368
Amanda Smith <ajs.amandasmith@gmail.com>
2022-07-06 21:08:59 -0500

    Fetches the current hardcover bestseller list

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import logo from '../logo.svg';
 import '../css/App.css';
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import Search from "./Search";
 import Filter from "./Filter";
 import BookListContainer from "./BookListContainer";
 
 function App() {
+  const api = process.env.REACT_APP_NYT_KEY;
 
-  const api = process.env.REACT_APP_NYT_KEY
+  const [bookData, setBookData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${api}`)
+      .then(r => r.json())
+      .then(books => {
+        setBookData(books.results.books)
+      })
+  }, [])
 
   return (
     <div>
@@ -15,7 +25,7 @@ function App() {
         The New York Times Reading List
       </header>
       <NavBar />
-      <BookListContainer />
+      <BookListContainer bookData={bookData} />
     </div>
 
     // <div className="App">
